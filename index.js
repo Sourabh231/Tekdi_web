1724c69847e7d49a3c6a016a94dcfeec0ad4122c
Sourabh Dudhale <sourabhdudhale@gmail.com>
2023-06-23 09:55:01 +0530

    file updated sucessfully

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+// var navbar = document.querySelector('.nav-bar-container')
+
+// window.onscroll = function() {
+
+//   // pageYOffset or scrollY
+//   if (window.pageYOffset > 0) {
+//     navbar.classList.add('scrolled')
+//   } else {
+//     navbar.classList.remove('scrolled')
+//   }
+// }
+
+window.addEventListener('scroll', function() {
+  var navbar = document.querySelector('.nav-bar-container');
+  if (window.scrollY > 0) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
+//-----------------------------------------------------/ 
+
+
 var swiper = new Swiper(".slide-content", {
     slidesPerView: 3,
     spaceBetween: 30,
